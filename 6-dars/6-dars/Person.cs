14e2c1391e6cf76604146bb2177e902843d643fa
TohirjonIbnAlisher <ibnalisher7379@gmail.com>
2024-06-12 21:53:13 +0500

    jsonSerializer added

---
+++ 6-dars/6-dars/Person.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace _6_dars;
+
+public class Person
+{
+    [JsonPropertyName("name")]
+    public string FullName { get; set; }
+    public int Age { get; set; }
+    public decimal Height { get; set; }
+    public decimal Weight { get; set; }
+}
